fddc873b8ba8978c4d9e6843ab044f33f81e3602
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-06-14 17:28:13 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,15 @@
+$(document).ready(function() {
+    var table = $('#data-table').DataTable();
+
+    $('#apply-filters').click(function() {
+        var filter1 = $('#filter1').val();
+        var filter2 = $('#filter2').val();
+        
+        $.get('/api/filter', { filter1: filter1, filter2: filter2 }, function(data) {
+            table.clear();
+            table.rows.add(data);
+            table.draw();
+        });
+    });
+});
 
