c7b4477a9db9ddf65a3f5e05d69c1858ab39313b
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-06-20 17:00:07 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -51,11 +51,5 @@ def index():
 def serve_static(filename):
     return send_from_directory(app.static_folder, filename)
 
-# Expose the WSGI application
-def handler(event, context):
-    from flask_lambda import FlaskLambda
-    lambda_app = FlaskLambda(app)
-    return lambda_app(event, context)
-
 if __name__ == '__main__':
     app.run(debug=True)
