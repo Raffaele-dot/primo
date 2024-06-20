a26f99125db0ddb0ae68d79e42807edd9d379ec3
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-06-20 10:41:53 +0200

    Update and rename __init__.py to __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, request, send_from_directory
+import pandas as pd
+import numpy as np
+import os
+
+app = Flask(__name__, static_folder='../static', template_folder='../static')
+
+# Load Excel data
+df = pd.read_excel('data.xlsx')
+
+# Endpoint to get columns
+@app.route('/api/columns', methods=['GET'])
+def get_columns():
+    columns = df.columns.tolist()
+    return jsonify(columns)
+
+# Endpoint to get unique values for a column
+@app.route('/api/column_values', methods=['GET'])
+def get_column_values():
+    column = request.args.get('column')
+    if column in df.columns:
+        values = df[column].dropna().unique().tolist()
+        return jsonify(values)
+    else:
+        return jsonify([]), 400
+
+# Endpoint to get filtered data
+@app.route('/api/filter', methods=['GET'])
+def filter_data():
+    query_params = request.args.to_dict(flat=False)
+    filtered_df = df.copy()
+
+    for column, values in query_params.items():
+        if column in df.columns:
+            filters = values[0].split('|')
+            regex_pattern = '|'.join(filters)
+            filtered_df = filtered_df[filtered_df[column].astype(str).str.contains(regex_pattern, case=False, na=False)]
+
+    filtered_df = filtered_df.replace({np.nan: None})  # Replace NaN with None
+    data = filtered_df.to_dict(orient='records')
+    return jsonify(data)
+
+# Serve index.html
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+# Serve other static files
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
