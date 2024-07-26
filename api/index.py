fcbb830e4473e8bd08e49622074e7acffb046fa9
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-07-26 16:05:33 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, send_from_directory
 import pandas as pd
-import os
 
 app = Flask(__name__, static_folder='../static')
 
@@ -12,41 +11,6 @@ df = pd.read_excel('data.xlsx')
 def get_columns():
     columns = df.columns.tolist()
     return jsonify(columns)
-
-# Endpoint to get unique values for a column
-@app.route('/api/column_values', methods=['GET'])
-def get_column_values():
-    column = request.args.get('column')
-    if column in df.columns:
-        values = df[column].dropna().unique().tolist()
-        return jsonify(values)
-    else:
-        return jsonify([]), 400
-
-# Endpoint to get filtered data
-@app.route('/api/filter', methods=['GET'])
-def filter_data():
-    query_params = request.args.to_dict(flat=False)
-    filtered_df = df.copy()
-
-    for column, values in query_params.items():
-        if column in df.columns:
-            filters = values[0].split('|')
-            exclude_filters = [f[1:].strip().lower() for f in filters if f.startswith('!')]
-            include_filters = [f.strip().lower() for f in filters if not f.startswith('!')]
-
-            if exclude_filters:
-                for filter_value in exclude_filters:
-                    filtered_df = filtered_df[~filtered_df[column].str.lower().str.contains(filter_value, na=False)]
-            
-            if include_filters:
-                include_mask = pd.Series([False] * len(filtered_df))
-                for filter_value in include_filters:
-                    include_mask |= filtered_df[column].str.lower().str.contains(filter_value, na=False)
-                filtered_df = filtered_df[include_mask]
-
-    data = filtered_df.replace({pd.NA: None}).to_dict(orient='records')
-    return jsonify(data)
 
 # Endpoint to get entire data
 @app.route('/api/data', methods=['GET'])
